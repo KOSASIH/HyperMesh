c1eb5b6557e0eb0f386fdfd747c79a68a9d1dcec
KOSASIH <kosasihg88@gmail.com>
2024-04-24 17:55:40 +0700

    Create hm_use_xml_commands.tcl

---
+++ func/hm_use_xml_commands.tcl
@@ -0,0 +1,41 @@
+# Import data object
+ImportDataObject my_data_object file_name.h5
+
+# Clear all data object properties
+ClearAllDataObjectProperties my_data_object
+
+# Clear all data object arguments
+ClearAllDataObjectArguments my_data_object
+
+# Run script file
+RunScriptFile file_name.tcl
+
+# Set data object expression
+SetDataObjectExpression my_data_object expr
+
+# Save data object with arguments
+SaveDataObjectWithArguments my_data_object arg1 arg2
+
+# Run session script file with arguments
+RunSessionScriptFileWithArguments my_session file_name.tcl arg1 arg2
+
+# Set plot property
+SetPlotProperty my_plot plot_property_value
+
+# Set data object property
+SetDataObjectProperty my_data_object property_value
+
+# Save plot
+SavePlot my_plot file_name.png
+
+# Save plot with arguments
+SavePlotWithArguments my_plot arg1 arg2
+
+# Save data object
+SaveDataObject my_data_object file_name.h5
+
+# Set plot title
+SetPlotTitle my_plot plot_title
+
+# Run session script with arguments
+RunSessionScriptWithArguments my_session arg1 arg2
