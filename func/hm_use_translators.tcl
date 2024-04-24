cab2162f6814d0bb4530942c406026982ce5d243
KOSASIH <kosasihg88@gmail.com>
2024-04-24 18:07:29 +0700

    Create hm_use_translators.tcl

---
+++ func/hm_use_translators.tcl
@@ -0,0 +1,77 @@
+# Import data object
+ImportDataObject my_data_object file_name.h5
+
+# Clear all data object properties
+ClearAllDataObjectProperties my_data_object
+
+# Clear all data object arguments
+ClearAllDataObjectArguments my_data_object
+
+# Run script file
+RunScriptFile file_name.tcl
+
+# Set data object expression
+SetDataObjectExpression my_data_object expr
+
+# Save data object with arguments
+SaveDataObjectWithArguments my_data_object arg1 arg2
+
+# Run session script file with arguments
+RunSessionScriptFileWithArguments my_session file_name.tcl arg1 arg2
+
+# Set plot property
+SetPlotProperty my_plot plot_property_value
+
+# Set data object property
+SetDataObjectProperty my_data_object property_value
+
+# Save plot
+SavePlot my_plot file_name.png
+
+# Save plot with arguments
+SavePlotWithArguments my_plot arg1 arg2
+
+# Save data object
+SaveDataObject my_data_object file_name.h5
+
+# Set plot title
+SetPlotTitle my_plot plot_title
+
+# Run session script with arguments
+RunSessionScriptWithArguments my_session arg1 arg2
+
+# Use the "SetDataObjectProperty" command with the "DataObject" and "Property" parameters to set a data object property.
+SetDataObjectProperty DataObject Property
+
+# Use the "RunSessionScriptWithArguments" command with the "Session" and "Arguments" parameters to run a session script with arguments.
+RunSessionScriptWithArguments Session Arguments
+
+# Use the "SavePlotWithArguments" command with the "Plot" and "Arguments" parameters to save a plot with arguments.
+SavePlotWithArguments Plot Arguments
+
+# Use the "SaveDataObjectWithArguments" command with the "DataObject" and "Arguments" parameters to save a data object with arguments.
+SaveDataObjectWithArguments DataObject Arguments
+
+# Use the "SetPlotTitle" command with the "Plot" and "Title" parameters to set a plot title.
+SetPlotTitle Plot Title
+
+# Use the "SetPlotProperty" command with the "Plot" and "Property" parameters to set a plot property.
+SetPlotProperty Plot Property
+
+# Use the "SavePlot" command with the "Plot" and "File" parameters to save a plot.
+SavePlot Plot File
+
+# Use the "RunScriptFile" command with the "File" parameter to run a script file.
+RunScriptFile File
+
+# Use the "ImportDataObject" command with the "DataObject" and "File" parameters to import a data object.
+ImportDataObject DataObject File
+
+# Use the "ClearAllDataObjectArguments" command with the "DataObject" parameter to clear all data object arguments.
+ClearAllDataObjectArguments DataObject
+
+# Use the "ClearAllDataObjectProperties" command with the "DataObject" parameter to clear all data object properties.
+ClearAllDataObjectProperties DataObject
+
+# Use the "RunSessionScriptFileWithArguments" command with the "Session" and "Arguments" parameters to run a session script file with arguments.
+RunSessionScriptFileWithArguments Session Arguments
